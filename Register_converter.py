1f71a95b8e5b62cebb21af60c6362b011b19153e
Aleks <alexeyz55555@gmail.com>
2022-11-26 22:30:09 +0200

    Add file

---
+++ Register_converter.py
@@ -0,0 +1,12 @@
+import pytest
+class NameConverter:
+
+    def __init__(self, word: str):
+        self.word = word
+
+    def to_snake_case(self):
+        pass
+
+    def to_camel_case(self):
+        pass
+
